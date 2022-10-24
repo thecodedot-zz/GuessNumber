7e8501bcff02ba84159cc3b07d7cbad68cc20890
thecodedot <72885755+thecodedot@users.noreply.github.com>
2022-10-24 13:48:54 +0500

    add number check function

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const x = function () {
+    console.log(23);
+}
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    // document.querySelector('.message').textContent = '🎉Correct Number!';
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!';
+    }
+});
